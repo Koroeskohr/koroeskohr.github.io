c90aa418f145dd9a3dea10fe4809bcd7c381a94f
Victor Viale <viale.victor@gmail.com>
2014-03-31 23:39:51 -0400

    portfolio

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	$(window).resize(function(){
-		$("header, section").height($(window).innerHeight());
-		$("header>div>img").height($(window).innerHeight()/2.5);
-	})
-	.trigger('resize');
+    $(window).resize(function() {
+        $("header").height($(window).innerHeight());
+        $("header>div>img").height($(window).innerHeight() / 2.5);
+    })
+        .trigger('resize');
 
 
 
